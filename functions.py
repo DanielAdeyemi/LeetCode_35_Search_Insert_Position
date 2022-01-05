07591ad8ea10e08f3f024e7085b38b8a0fe7e9ea
DanielAdeyemi <adeyemidany@gmail.com>
2022-01-04 20:03:46 -0500

    binary search solution in Python

--- functions.py
+++ functions.py
@@ -6,3 +6,19 @@ class Solution:
                 return i
             elif(nums[i] < target and i == len(nums) - 1):
                 return i+1
+
+
+class SolutionTwo:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # binary search:
+        left, right = 0, len(nums) - 1
+        while(left <= right):
+            middle = (left + right) // 2
+            if (nums[middle] == target):
+                return middle
+            elif (nums[middle] > target):
+                right = middle - 1
+            else:
+                left = middle + 1
+
+        return left
