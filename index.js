32174d5c51bab30bb69920e4b09f62335c79f364
DanielAdeyemi <adeyemidany@gmail.com>
2022-01-04 20:04:49 -0500

    add binary search solution in js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // brute force:
-var searchInsert = function (nums, target) {
+const searchInsert = function (nums, target) {
 	for (let i = 0; i < nums.length; i++) {
 		if (nums[i] === target || nums[i] > target) {
 			return i;
@@ -8,4 +8,19 @@ var searchInsert = function (nums, target) {
 		}
 	}
 	return 0;
+};
+
+const searchInsertTwo = function (nums, target) {
+	//binary search:
+	let left = 0;
+	let right = nums.length - 1;
+	while (left <= right) {
+		let middle = Math.ceil((left + right) / 2);
+		if (nums[middle] === target) return middle;
+		else if (nums[middle] < target) {
+			left = middle + 1;
+		} else if (nums[middle] > target) {
+			right = middle - 1;
+		}
+	}
 };
